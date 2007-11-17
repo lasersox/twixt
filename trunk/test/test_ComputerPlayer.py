51f94c3c629aa6b875a0dea5e02f19ae85dbf850
alex.j.ross <alex.j.ross@57d76746-2f3d-0410-a78d-4b6d76877d32>
2007-11-17 23:44:27 +0000

    IT WORKS!

---
+++ trunk/test/test_ComputerPlayer.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.getcwd())
+
+from pytwixt import node_twixt as twixt
+from index import render_game_board_image, ComputerPlayer
+
+game = twixt.Twixt("muzi", "thanh", (10, 10))
+
+c1 = ComputerPlayer("muzi")
+c2 = ComputerPlayer("thanh")
+players = {"muzi": c1, "thanh":c2}
+
+turn_number = 0
+game.current_player = game.player2
+while True:
+
+    xy = players[game.current_player].next_move(game)
+    game.claim_node(xy, game.current_player)
+    
+    game.id = "test_cp_%i" % turn_number
+    #render_game_board_image(game)
+    turn_number += 1
+
+    if game.has_won(game.current_player):
+        #print "%s HAS WON THE GAME!\n" % game.current_player
+        break
+    else:
+        game.current_player = game.opponent(game.current_player)
+    
+
+
+
--- trunk/twixt_heuristic.py
+++ trunk/twixt_heuristic.py
@@ -1,11 +1,12 @@
 from math import *
+from pytwixt import node_twixt as twixt
+import copy
 
 def f_1(game, player):
     """ Compute total length of bridges for player. """
     conn_len = sqrt(1 + 2.**2)
     total = 0
-    for conn in game.connections:
-        if conn.p0.owner == player and conn.p1.owner == player:
+    for conn in game.connections(player):
             total += conn_len
     return total / (game.size[0] * game.size[1])
 
@@ -36,6 +37,8 @@ def f_3(game, player):
         conn_bridges.add(len(conn_nodes)-1)
         
     """ checking for longest path """
+    if not conn_bridges:
+        return 0
     return max(conn_bridges)/(game.size[0]*game.size[1])
 
 def f_4(game, player):
@@ -47,9 +50,9 @@ def f_4(game, player):
     for bridge in bridges:
         vnodes[bridge.p0.y] = 1
         vnodes[bridge.p1.y] = 1
-        vnodes[round((bridge.p0.y + bridge.p1.y)/2)] = 1
+        vnodes[int(round((bridge.p0.y + bridge.p1.y)/2))] = 1
     
-    return sum(vnodes)/(game.size[0]*game.size[1])
+    return float(sum(vnodes)/(game.size[0]*game.size[1]))
 
 
 def f_5(game, player):
@@ -61,35 +64,34 @@ def f_5(game, player):
     for bridge in bridges:
         hnodes[bridge.p0.x] = 1
         hnodes[bridge.p1.x] = 1
-        hnodes[round((bridge.p0.x + bridge.p1.x)/2)] = 1
+        hnodes[int(round((bridge.p0.x + bridge.p1.x)/2))] = 1
     
-    return sum(hnodes)/(game.size[0]*game.size[1])
+    return float(sum(hnodes)/(game.size[0]*game.size[1]))
 
 
 def f_6(game, player):
     """ Return the reciprocal of player's f_4. """
-    return 1 / f_4(game, player)
+    return 1. / (f_4(game, player)+1)
 
 def f_7(game, player):
     """ Return the reciprocal of player's f_5. """
-    return 1 / f_5(game, player)
+    return 1. / (f_5(game, player) + 1)
 
 def f_8(game, player):
     """ Evaluate the ability to extend the current bridges """
     ext_bridges = 0
 
-    for node0 in game.nodes:
-            if node0.owner == player.name:
-                for node1 in game.nodes:
-                    if node1.owner == "" and node1.reservee != game.opponent(player).name:
-                        xdif, ydif = abs(node0.x - node1.x), abs(node0.y - node1.y)
-                        if ((xdif != 1 or ydif != 2) and (ydif != 1 or xdif != 2))== False:                     
-                            """ check for intersection """
-                            conn = Connection(node0, node1)
-                            for other_conn in self.connections:
-                                if intersects(conn, other_conn) == False:
-                                    ext_bridges += 1
-        
+    for node0 in game.nodes.itervalues():
+        if node0.owner == player.name:
+            for node1 in game.nodes.itervalues():
+                if node1.owner == "" and node1.reservee != game.opponent(player):
+                    xdif, ydif = abs(node0.x - node1.x), abs(node0.y - node1.y)
+                    if ((xdif != 1 or ydif != 2) and (ydif != 1 or xdif != 2))== False:                     
+                        """ check for intersection """
+                        conn = twixt.Connection(node0, node1)
+                        for other_conn in game.connections(game.opponent(player)):
+                            if twixt.intersects(conn, other_conn) == False:
+                                ext_bridges += 1
     return ext_bridges      
     
     
@@ -97,39 +99,47 @@ def get_next_states(game,player,depth):
     """ generate all possible game states at depth ahead 
     1. Notice, make sure return (game,node) 
     Comment: Using array to store the tree: A node will be an array of [[Current],[Childrent],[Move]]"""
-    parrent = [game,[], []]
-    parrent = get_next_recur_states(parrent, player, depth)
-    return parrent      
+    parent = [game, [], None]
+    parent = get_next_recur_states(parent, player, depth)
+    return parent      
 
-def get_next_recur_states(parrent, player, depth):
+def get_next_recur_states(parent, player, depth):
     """ Using recursion to generate next states recursively 
     Note: a node is [game, chidren, move]"""
     
     # get all the valid nodes for this player
-    valid_nodes = get_valid_nodes(parrent[0],player)
-        
     # This is the leaf node
     if depth == 1:  
+        valid_nodes = get_valid_nodes(parent[0],player)
         for valid_node in valid_nodes:
             # get a copy the current game board
-            temp_game = parrent[0]
+            temp_game = copy.deepcopy(parent[0])
             # claim the avalaible node
-            temp_game.claim_node((valid_node.x, valid_node.y),player)
+            try:
+                temp_game.claim_node((valid_node.x, valid_node.y), player)
+            except twixt.NodeError, e:
+                print player, valid_node.reservee
+                raise e
             # add the new game board to as the child to the current node
-            parrent[1].append([temp_game,[],valid_node])    
+            parent[1].append([temp_game,[],valid_node])    
     else: #if not leaf, call recursively
+        # determine which player is in turn
+        valid_nodes = get_valid_nodes(parent[0], player)
         for valid_node in valid_nodes:
-            # determine which player is in turn
-            if depth%2 == 0:
-                cur_player = parrent[0].opponent(player)
-                
-            temp_states = get_next_recur_states(parrent,cur_player,depth-1)
-            for temp_state in temp_states:
-                parrent[1].append(temp_state) 
+            # get a copy the current game board
+            temp_game = copy.deepcopy(parent[0])
+            # claim the avalaible node
+            try:
+                temp_game.claim_node((valid_node.x, valid_node.y), player)
+            except twixt.NodeError, e:
+                print player, valid_node.reservee
+                raise e
+            temp_states = get_next_recur_states([temp_game, [], valid_node], parent[0].opponent(player), depth-1)
+            parent[1].append(temp_states) 
     
-    return parrent
+    return parent
 
-def get_next_loo_states(parrent, player, depth):
+def get_next_loo_states(parent, player, depth):
     for i in range(depth):
        
        pass
@@ -138,9 +148,11 @@ def get_valid_nodes(game,player):
     """ return the valid nodes for a player """
     valid_nodes = set()
     for node in game.nodes.itervalues():
-        if node.owner == "" or node.reservee == player:
+        if node.reservee == game.opponent(player):
+            continue
+        if node.owner == "" and node.reservee != game.opponent(player):
             valid_nodes.add(node)
     return valid_nodes
- 
-    
+
+
 
